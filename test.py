d41d96a778b9e750c954f4e340d0b07ffbad6048
duydung-14122025 <quandangminh0721@gmail.com>
2025-12-28 15:21:13 +0700

    uppdate code

---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+
+TARGET_DIR = "assets/img/full/bridgegroom"  # đổi thành thư mục của bạn
+
+for root, _, files in os.walk(TARGET_DIR):
+    for file in files:
+        if file.lower().endswith(".webp"):
+            new_name = file.replace("_", "")
+            if new_name != file:
+                old_path = os.path.join(root, file)
+                new_path = os.path.join(root, new_name)
+
+                if not os.path.exists(new_path):
+                    os.rename(old_path, new_path)
+                    print(f"✔ {file} → {new_name}")
+                else:
+                    print(f"⚠ Bỏ qua (trùng tên): {new_name}")
